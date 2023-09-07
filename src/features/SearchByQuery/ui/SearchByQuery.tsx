8e5379653a94516000845e2ec2d59a5c66979d0b
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-09-07 21:48:18 +0300

    day 2 changes: search by query fixes

--- src/features/SearchByQuery/ui/SearchByQuery.tsx
+++ src/features/SearchByQuery/ui/SearchByQuery.tsx
@@ -1,32 +1,61 @@
 import { FC, useState } from 'react'
-import { useAppDispatch } from '@/shared/libs/hooks/useAppDispatch'
 import { reciveAllBooks } from '@/entities/book'
 import { Input } from '@/shared/ui/input/Input'
+import {
+  resetRecivedData,
+  selectState,
+  setPrevSearchParams,
+  setSearchQuery,
+} from '@/entities/book/model/slice/bookSlice'
+import { SearchParams } from '@/entities/book/api/types'
+import { Button } from '@/shared/ui/button'
 import cls from './SearchByQuery.module.scss'
-import { setSearchQuery } from '@/entities/book/model/slice/BookSlice'
+import { useAppDispatch, useAppSelector } from '@/app/providers/redux'
 
 interface SearchByQueryProps {}
 
-export const SearchByQuery: FC = (props) => {
+export const SearchByQuery: FC = () => {
   const [inputValue, setInputValue] = useState('')
+  const { searchQuery, selectedSort, selectedCategory } = useAppSelector(selectState)
   const dispatch = useAppDispatch()
 
+  console.log('input', inputValue)
+  console.log('search', searchQuery)
+
+  console.log('category:', selectedCategory)
+
+  const searchParams: SearchParams = {
+    query: inputValue,
+    sort: selectedSort,
+    subject: selectedCategory,
+    startIndex: 0,
+  }
+
+  const clickCondition = inputValue.trim() !== '' && inputValue.trim() !== ' '
+
   const handleClickButton = () => {
-    if (inputValue.trim() !== '') {
+    if (clickCondition) {
+      if (inputValue === searchQuery) {
+        dispatch(resetRecivedData())
+      }
+
       dispatch(setSearchQuery(inputValue))
-      dispatch(reciveAllBooks({ query: inputValue, page: 0 }))
-      setInputValue('')
+      dispatch(setPrevSearchParams(searchParams))
+      dispatch(reciveAllBooks(searchParams))
     }
   }
 
   return (
-    <>
+    <div className={cls.search}>
       <Input
+        type='text'
         placeholder='What book would you like to find ?'
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={handleClickButton}>OK</button>
-    </>
+      <Button theme='primary' onClick={handleClickButton} disabled={!clickCondition}>
+        Search
+      </Button>
+    </div>
   )
 }
