9682674a5aff3f79d7e33b30dd63fbd95a06e25b
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-09-07 21:50:56 +0300

    day 2 changes: add search params type

--- src/entities/book/api/types.ts
+++ src/entities/book/api/types.ts
@@ -1,7 +1,15 @@
-import { Book } from "../model/types/Book"
+import { CategoryType, SortType } from '../model/slice/bookSlice.types'
+import { Book } from '../model/types/Book'
 
 export interface ApiResponse {
-	kind: string
-	totalItems: number
-	items: Book[]
+  kind: string
+  totalItems: number
+  items: Book[]
+}
+
+export interface SearchParams {
+  query: string
+  subject?: CategoryType
+  sort?: SortType
+  startIndex: number
 }
