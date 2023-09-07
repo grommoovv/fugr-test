c2feb73ba6afffe6cca1cb6d65de14eb392513dc
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-09-07 21:34:28 +0300

    day 2 changes: fix

--- src/shared/api/axios/axios.ts
+++ src/shared/api/axios/axios.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
-import { __API_KEY__, __API__ } from '../keys'
+import { __API__ } from '../keys'
 
 export const $axios = axios.create({
   baseURL: __API__,
   headers: {
     'Content-Type': 'application/json',
-    // key: __API_KEY__,
   },
 })
