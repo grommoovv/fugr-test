893d4871b99e21890f87026e7145b2d38bb923ae
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-09-08 14:28:03 +0300

    day 3 changes: fixes

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,18 +1,23 @@
 import { FC } from 'react'
 import { SearchForm } from '@/widgets/SearchForm'
+import { useLocation } from 'react-router'
 import cls from './Header.module.scss'
 
 interface HeaderProps {}
 
 export const Header: FC = (props) => {
-  return (
-    <>
+  const location = useLocation()
+
+  if (location.pathname === '/') {
+    return (
       <header className={cls.header}>
         <div className={cls.container}>
           <h1 className={cls.title}>Search For Books</h1>
           <SearchForm />
         </div>
       </header>
-    </>
-  )
+    )
+  }
+
+  return null
 }
