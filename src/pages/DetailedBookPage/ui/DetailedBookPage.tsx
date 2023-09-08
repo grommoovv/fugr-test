b7be46e0e84438a581b137e2063105c5abf6bdf7
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-09-08 14:24:51 +0300

    day 3 changes: builded detailed book page

--- src/pages/DetailedBookPage/ui/DetailedBookPage.tsx
+++ src/pages/DetailedBookPage/ui/DetailedBookPage.tsx
@@ -1,12 +1,57 @@
-import { FC } from 'react'
+import { Book, BookService } from '@/entities/book'
+import { volumeInfo } from '@/entities/book/model/types/Book'
+import { BookDetails } from '@/entities/book/ui/BookDetails'
+import { Loader } from '@/shared/ui/loader'
+import { FC, useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 // import cls from './DetailedBookPage.module.scss'
 
 interface DetailedBookPageProps {}
 
 const DetailedBookPage: FC = (props) => {
+  const { id } = useParams()
+
+  const [book, setBook] = useState<volumeInfo | null>(null)
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const reciveBook = async () => {
+      try {
+        if (id) {
+          const res = await BookService.reciveOneBook(id)
+          setBook(res.volumeInfo)
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message)
+        }
+        setError('Failed To Load Data')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    reciveBook()
+  }, [])
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <div>{error}</div>
+  }
+
   return (
     <>
-      <div className={''}>DetailedBookPage</div>
+      {book ? (
+        <BookDetails book={book} />
+      ) : (
+        <div>
+          <div>Loading Failed</div>
+        </div>
+      )}
     </>
   )
 }
